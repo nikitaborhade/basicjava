0bcd693f9f865f6df656c6292f60ed51dd6fed74
Nikita <Nikita@DESKTOP-261P2DR>
2019-07-28 20:50:34 +0530

    First push

---
+++ Basic Java/src/com/example/array/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.array;
+
+/**
+ * @author Nikita Borhade
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+	}
+
+}
